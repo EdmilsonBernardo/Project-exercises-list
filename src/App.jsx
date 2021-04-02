63d98a2f025f718d2f6948737859955fd4ef0f8c
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-04-01 21:44:21 -0300

    Adiciona exercício 5.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import React from 'react'
 
-import Primeiro from './components/Primeiro'
+//import Primeiro from './components/Primeiro'
 import ComParamentro from './components/ComParamentro'
-import ComFilhos from './components/ComFilhos'
+//import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/Card'
 
 export default (props) => (
@@ -20,9 +20,13 @@ export default (props) => (
             <ComParamentro titulo="Função horária do MUV"
                 subtitulo="Um móvel parte do repouso e desenvolve uma aceleração constante de 3 m/s² durante 4 segundos. De quantos metros foi o deslocamento desse móvel?" />
         </Card>
-        <Card titulo="Exercício - #03" color="#688F4E" downColor="F4F1E9">
+        <Card titulo="Exercício - #04" color="#688F4E" downColor="F4F1E9">
             <ComParamentro titulo="Análise dimensional"
                 subtitulo="A grandeza física potência, medida em Watts, nas unidades do S.I., mede a quantidade de trabalho, em Joules, que é realizado a cada segundo. Como a unidade Watt também pode ser escrita?" />
+        </Card>
+        <Card titulo="Exercício - #05" color="#688F4E" downColor="F4F1E9">
+            <ComParamentro titulo="Calor latente"
+                subtitulo="Um corpo de massa 6g em estado sólido, é aquecido até o ponto de fusão. Sabendo que o calor latente do corpo é de 35 cal/g, determine a quantidade de calor recebida pelo corpo." />
         </Card>
     </div>
 );
